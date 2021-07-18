17d1908a880ea464d1cecdd37960b79ccb6f8a70
x-1337-x <g1thub_acc@protonmail.com>
2021-07-18 17:42:44 +0300

    nanoID, error handling

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const PORT = 8889;
 
 const app = express();
 
+const { nanoid } = require('nanoid');
+
 class DataBase {
 	constructor() {
 		this.storage = [];
-		this.id = 1;
 	}
 
 	getRecord(alias) {
@@ -16,13 +17,24 @@ class DataBase {
 		});
 	}
 
-	createAlias(url) {
-		let alias = String(this.id++);
+	createAlias() {
+		let alias = nanoid(7);
 		return alias;
 	}
 
 	createRecord(url) {
-		let record = { url, alias: this.createAlias(url) };
+		let alias;
+		let counter = 0;
+		let found;
+		do {
+			counter++;
+			alias = this.createAlias();
+			found = this.getRecord(alias);
+		} while (counter < 100 && found);
+		if (found) {
+			throw new Error('failed to geterate a unique alias');
+		}
+		let record = { url, alias };
 		this.storage.push(record);
 		return record;
 	}
@@ -46,7 +58,7 @@ app.get('/', function (req, res) {
 app.get('/:alias', (req, res) => {
 	let url = DB.getRecord(req.params.alias);
 	if (url) {
-		return res.send(url);
+		return res.redirect(301, url.url);
 	} else {
 		return res.status(400).end();
 	}
@@ -54,9 +66,13 @@ app.get('/:alias', (req, res) => {
 
 app.post('/', (req, res) => {
 	if (req.body.url) {
-		const record = DB.createRecord(req.body.url);
-		const message = `Here is your short link: <a href="http://localhost:8889/${record.alias}">http://localhost:8887/${record.alias}</a>`;
-		return res.send(message);
+		try {
+			const record = DB.createRecord(req.body.url);
+			const message = `Here is your short link: <a href="http://localhost:8889/${record.alias}">http://localhost:8887/${record.alias}</a>`;
+			return res.send(message);
+		} catch (error) {
+			return res.status(500).end();
+		}
 	} else {
 		return res.status(400).end();
 	}
